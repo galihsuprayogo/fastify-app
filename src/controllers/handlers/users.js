23e892f36d93cadc161561af1373eda030a7228c
Galih Suprayogo <suprayogogalih@gmail.com>
2024-03-05 08:41:52 +0700

    Add bcrypt

--- src/controllers/handlers/users.js
+++ src/controllers/handlers/users.js
@@ -1,14 +1,17 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import { fastifyDB } from '../../configs/index.js'
 
 const { sign } = jwt
+const { hashSync, compareSync } = bcrypt
 
 export const registerUserHandler = (req, reply) => {
   const { username, email, password } = req.body
+  const encryptedPassword = hashSync(password, Number(process.env.SALT_ROUNDS))
 
   fastifyDB.fastifyDB.mysql.query(
     'INSERT INTO USERS (username, email, password) VALUES (?, ?, ?)',
-    [username, email, password],
+    [username, email, encryptedPassword],
     function onResult(err, result) {
       reply.send(
         err || {
@@ -23,13 +26,19 @@ export const loginUserHandler = (req, reply) => {
   const { username, password } = req.body
 
   fastifyDB.fastifyDB.mysql.query(
-    'SELECT * FROM USERS WHERE username=? AND password=?',
-    [username, password],
+    'SELECT * FROM USERS WHERE username=?',
+    [username],
     function onResult(err, result) {
       if (result.length !== 0 && result) {
-        const token = sign({ username }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' })
-        const arr = [{ ...result[0], token }]
-        reply.send(arr)
+        const comparePwd = compareSync(password, result[0].password)
+        if (comparePwd) {
+          delete result[0].password
+          const token = sign({ username }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' })
+          const arr = [{ ...result[0], token }]
+          reply.send(arr)
+        } else {
+          reply.send({ message: 'Invalid password' })
+        }
       } else {
         reply.send({ message: 'User not found' })
       }
